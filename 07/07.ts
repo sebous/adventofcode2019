c2b30cf7ea04ad9c6291ac0c80c408768c955e97
Krepela, Sebastian <skrepela@netsuite.com>
2019-12-10 17:28:44 +0100

    fixed intcode computer

--- 07/07.ts
+++ 07/07.ts
@@ -1,32 +1,53 @@
 import path from "path";
 import _ from "lodash";
-import readline from "readline-sync";
 import { permutations, readInputFile } from "../util";
 import { intcodeComputer } from "../intcode";
 
+function part1(input: string) {
+    const inputProgram = input.split(',').map(Number);
+
+    const output = permutations([0, 1, 2, 3, 4]).reduce((output: number, ampsPhase: number[]): number => {
+        let amplifierOutput: number[] = []
+        _.range(5).forEach(i => {
+            let isFirstInput = true
+
+            intcodeComputer(
+                [...inputProgram],
+                0,
+                () => {
+                    const input = isFirstInput ? ampsPhase[i] : (amplifierOutput[i - 1] || 0)
+                    isFirstInput = false
+                    return input
+                },
+                (op: number) => {
+                    amplifierOutput[i] = op
+                },
+            );
+
+        });
+        return amplifierOutput[4] > output ? amplifierOutput[4] : output
+    }, 0)
+    return output;
+}
 
 (async () => {
     const input: string = await readInputFile(path.join(__dirname + '/data.txt'));
-    const inputProgram = input.split(',').map(Number);
+    const output1 = part1(input);
 
-    permutations([0, 1, 2, 3, 4]).reduce((output: number, phase: []): number => {
-        const amplifierOutput = [];
+    console.log(output1);
 
-        _.range(0, 5).forEach(i => {
-            let isFirstInput = true;
-            intcodeComputer(
+    // const testInputData: string = await readInputFile(path.join(__dirname, '/data_test.txt'));
+    // const testInput = testInputData.split(',').map(Number);
+    // // console.log(testInput);
+    // const testOutput = intCodeComputer(
+    //     [...testInput],
+    //     0,
+    //     () => {
+    //         const input = readline.question('>> INPUT:') || '1';
+    //         return Number(input);
+    //     },
+    //     (op: number) => console.log(op),
+    // );
 
-            )
-        })
-    })
-
-    intcodeComputer(
-        [...inputProgram],
-        0,
-        () => {
-            const input = readline.question('>> INPUT: ');
-            return Number(input);
-        },
-        (op: number) => console.log(op),
-    );
+    // console.log(testOutput);
 })();
--- intcode.ts
+++ intcode.ts
@@ -1,5 +1,5 @@
-type UserInputCallback = () => number;
-type OutputCallback = (op: number) => void;
+type InputFn = () => number;
+type OutputFn = (op: number) => void;
 
 interface IntcodeComputerResponse {
     program: number[];
@@ -11,7 +11,7 @@ interface IntcodeComputerResponse {
 const POSITION = 0;
 const IMMEDIATE = 1;
 
-export function intcodeComputer(program: number[], currentIndex: number, userInput: UserInputCallback, output: OutputCallback): IntcodeComputerResponse {
+export function intcodeComputer(program: number[], currentIndex: number, userInput: InputFn, output: OutputFn): IntcodeComputerResponse {
     const opCode = program[currentIndex]
         .toString()
         .padStart(5, '0')
@@ -19,62 +19,67 @@ export function intcodeComputer(program: number[], currentIndex: number, userInp
         .map(Number);
 
     if (opCode[4] === 9 && opCode[3] === 9) {
-        return { program, currentIndex, isHalted: true, isPaused: false };
+        return { program: program, currentIndex, isHalted: true, isPaused: false }
     }
 
     const modes = [
         opCode[2] === 0 ? POSITION : IMMEDIATE,
         opCode[1] === 0 ? POSITION : IMMEDIATE,
         opCode[0] === 0 ? POSITION : IMMEDIATE,
-    ];
+    ]
 
     const params = program.slice(currentIndex + 1, currentIndex + 4);
 
     const getParam = (paramIndex: number) => modes[paramIndex] === POSITION ? program[params[paramIndex]] : params[paramIndex];
 
-    switch (opCode[4]) {
-        case 1: {
-            const first = getParam(0);
-            const second = getParam(1);
-            program[params[2]] = first + second;
-            return intcodeComputer(program, currentIndex + 4, userInput, output);
-        }
-        case 2: {
-            const first = getParam(0);
-            const second = getParam(1);
-            program[params[2]] = first + second;
-            return intcodeComputer(program, currentIndex + 4, userInput, output);
-        }
-        case 3: {
-            const input = userInput();
-            if (typeof input === 'undefined') {
-                return { program, currentIndex, isHalted: false, isPaused: true };
-            }
+    if (opCode[4] === 1) {
+        program[params[2]] = getParam(0) + getParam(1);
 
-            program[params[0]] = Number(input);
-            return intcodeComputer(program, currentIndex + 2, userInput, output);
-        }
-        case 4: {
-            const op = getParam(0);
-            output(op);
-            return intcodeComputer(program, currentIndex + 2, userInput, output);
-        }
-        case 5: {
-            const nextIndex = getParam(0) !== 0 ? getParam(1) : currentIndex + 3;
-            return intcodeComputer(program, nextIndex, userInput, output);
-        }
-        case 6: {
-            const nextIndex = getParam(0) === 0 ? getParam(1) : currentIndex + 3;
-            return intcodeComputer(program, nextIndex, userInput, output);
-        }
-        case 7: {
-            program[params[2]] = getParam(0) < getParam(1) ? 1 : 0;
-            return intcodeComputer(program, currentIndex + 4, userInput, output);
-        }
-        case 8: {
-            program[params[2]] = getParam(0) === getParam(1) ? 1 : 0;
-            return intcodeComputer(program, currentIndex + 4, userInput, output);
-        }
+        return intcodeComputer(program, currentIndex + 4, userInput, output)
     }
-    throw new Error(`Wrong opCode: ${opCode.join('')}, c: ${currentIndex}`);
+
+    if (opCode[4] === 2) {
+        program[params[2]] = getParam(0) * getParam(1);
+
+        return intcodeComputer(program, currentIndex + 4, userInput, output)
+    }
+
+    if (opCode[4] === 3) {
+        const input = userInput()
+        if (typeof input === "undefined") {
+            return { program: program, currentIndex, isHalted: false, isPaused: true }
+        }
+
+        program[params[0]] = input
+
+        return intcodeComputer(program, currentIndex + 2, userInput, output)
+    }
+
+    if (opCode[4] === 4) {
+        const op = getParam(0);
+        output(op);
+        return intcodeComputer(program, currentIndex + 2, userInput, output);
+    }
+
+    if (opCode[4] === 5) {
+        const nextIndex = getParam(0) !== 0 ? getParam(1) : currentIndex + 3;
+        return intcodeComputer(program, nextIndex, userInput, output);
+    }
+
+    if (opCode[4] === 6) {
+        const nextIndex = getParam(0) === 0 ? getParam(1) : currentIndex + 3;
+        return intcodeComputer(program, nextIndex, userInput, output);
+    }
+
+    if (opCode[4] === 7) {
+        program[params[2]] = getParam(0) < getParam(1) ? 1 : 0;
+        return intcodeComputer(program, currentIndex + 4, userInput, output);
+    }
+
+    if (opCode[4] === 8) {
+        program[params[2]] = getParam(0) === getParam(1) ? 1 : 0;
+        return intcodeComputer(program, currentIndex + 4, userInput, output);
+    }
+
+    throw new Error("Something went wrong." + opCode.join("") + "c: " + currentIndex)
 }
