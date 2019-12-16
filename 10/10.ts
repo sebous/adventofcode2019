cf1d19ebc4d3c90fa237b8b147de4f72a0e425b1
Krepela, Sebastian <skrepela@netsuite.com>
2019-12-16 15:42:19 +0100

    progress 10p2

--- 10/10.ts
+++ 10/10.ts
@@ -7,6 +7,11 @@ interface Coords {
     y: number;
 }
 
+interface Position extends Coords {
+    distance: number;
+    angle: number;
+}
+
 interface Space {
     map: SpaceMap;
     height: number;
@@ -14,6 +19,7 @@ interface Space {
 }
 
 type SpaceMap = { [key: string]: Point };
+type SpacePositionMap = { [key: string]: Position };
 
 enum Point {
     EMPTY = '.',
@@ -30,12 +36,26 @@ const getCoords = (str: string): Coords => {
 /**
  * Is C between A and B?
  */
-function isBetween(C: Coords, A: Coords, B: Coords) {
-    return (distance(A, C) + distance(B, C)) === distance(A, B);
+function isBetween(C: string, A: string, B: string) {
+    const tolerance = 0.000001;
+    const c = getCoords(C);
+    const a = getCoords(A);
+    const b = getCoords(B);
+    return Math.abs(distance(a, b) - distance(b, c) - distance(a, c)) <= tolerance;
 }
 
 function distance(A: Coords, B: Coords) {
     return Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+}
+
+function calculateAngle(target: string, center: string) {
+    const targetCoords = getCoords(target);
+    const centerCoords = getCoords(center);
+
+    const longSide = Math.sqrt(Math.pow(targetCoords.x - centerCoords.x, 2) + Math.pow(targetCoords.y - centerCoords.y, 2));
+    const angleAlpha = targetCoords.x / longSide;
+    const degrees = angleAlpha * (180 / Math.PI);
+    return degrees;
 }
 
 function createSpace(rawMapData: string): Space {
@@ -56,29 +76,78 @@ function createSpace(rawMapData: string): Space {
 
 function partOne(space: Space) {
     const asteroids = Object.entries(space.map).filter(([coord, point]) => point === Point.ASTEROID);
-    const stationCandidates = asteroids
+    const stationCandidates: [string, number][] = asteroids
         .map(([coord, point]) => {
             const otherAsteroids = asteroids.filter(([c]) => c !== coord);
             const visible = otherAsteroids.filter(([currCoord, currPoint]) => {
                 const remainingAsteroids = otherAsteroids.filter(([c]) => c !== currCoord);
-                return remainingAsteroids.every(ast => !isBetween(getCoords(ast[0]), getCoords(coord), getCoords(currCoord)));
+                return remainingAsteroids.every(ast => !isBetween(ast[0], coord, currCoord));
             });
             return [coord, visible.length];
         });
-    const bestCandidate = _.maxBy(stationCandidates, candidate => candidate[1]);
+    const bestCandidate: [string, number] | undefined = _.maxBy(stationCandidates, candidate => candidate[1]);
+    if (!bestCandidate) throw new Error('nothing found');
     console.log(bestCandidate);
+    return bestCandidate;
+}
+
+function partTwo(space: Space, [stationCoords, asteroidCount]: [string, number]) {
+    const asteroids = Object.entries(space.map)
+        .filter(([coord, point]) => point === Point.ASTEROID)
+        .filter(([coord]) => coord !== stationCoords)
+        .map(([coord]) => (<Position>{
+            x: getCoords(coord).x,
+            y: getCoords(coord).y,
+            distance: distance(getCoords(stationCoords), getCoords(coord)),
+            angle: calculateAngle(coord, stationCoords),
+        }));
+
+    const asteroidMap: SpacePositionMap = {};
+    asteroids.forEach(ast => asteroidMap[`${ast.x}, ${ast.y}`] = ast);
+    console.log(asteroidMap);
+
+    let asteroidCounter = 1;
+    let currentAngle = 0;
+    while (Object.keys(asteroidMap).length > 0) {
+        const currentTargets = Object.entries(asteroidMap).filter(([coord, pos]) => pos.angle === currentAngle);
+        const closestTarget = _.minBy(currentTargets, (([coord, pos]) => pos.distance));
+
+        if (!closestTarget) throw new Error('wrong next target calculation');
+
+        // console.log(`fire in the hole: ${closestTarget[0]}, count: ${asteroidCounter}`);
+        delete asteroidMap[closestTarget[0]];
+        asteroidCounter += 1;
+
+        // find next target angle
+        const remainingTargets = Object.entries(asteroidMap);
+        const nextAsteroid = _.minBy(remainingTargets, (([coord, pos]) => {
+            // if nothing left under 360deg, find first in next round
+            if (!remainingTargets.some(([coord, pos]) => pos.angle > currentAngle)) {
+                return pos.angle;
+            }
+            // get closest angle
+            return pos.angle - currentAngle;
+        }));
+        if (!nextAsteroid) throw new Error('no asteroids left??');
+
+        currentAngle = nextAsteroid[1].angle;
+    }
 }
 
 (async () => {
     const rawMapData: string = await readInputFile(path.join(__dirname, '/data.txt'));
     const space = createSpace(rawMapData);
-    partOne(space);
-    tests();
+    const station = partOne(space);
+    partTwo(space, station);
+    // tests();
 })()
 
 
 function tests() {
     const B = '1,0';
     const A = '3,4';
+
+    console.log(isBetween('2,2', '1,0', '3,4'));
+    console.log(isBetween('3,2', '1,0', '4,3'));
 
 }
